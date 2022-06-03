e18ec7ca4373449a2859c0878840870464994ab7
klibin <km.putnam2@gmail.com>
2022-06-03 14:59:57 -0600

    Configure React app for deployment to GitHub Pages

---
+++ README.md
@@ -0,0 +1,32 @@
+# Memory Card Game
+## Udemy React Course Project
+
+This is a classic card memory game - find the two matching cards, and the cards will stay face up. If they don't match, they will flip back over. There is a counter at the bottom of the screen that tracks the amount of turns you've taken, and it resets when you shuffle the deck.
+
+### Languages
+
+React, JavaScript, CSS, HTML
+
+### Project Highlights
+
+This project was designed so that students could get a better understanding of how basic React apps work. It did this via:
+
+    1. Using useState and useEffect hooks
+    2. Passing and recieving props
+    3. Implementing basic JavaScript logic to evaluate if cards matched, and what to do if the cards did (or didn't) match
+    4. Creating dynamic classNames to house matched cards
+
+
+### What I learned / Challenges
+
+I enjoyed this project because it allowed me to see how React Developers go about actually figuring out where to start when building a project. 
+Lots of Codecademy projects throw you in to a half built project, which is fine, but as I've been learning I have noticed it is harder to get a holistic understanding of how to build and manipulate a project if half of it is written for you. I mean, when you look at a game, how do you know which bit to start with? Being able to see and hear how an experienced developer breaks down each element and get started was extremely helpful. 
+
+As this was the first project I've done from the ground up, it definitely took me a bit of time to put all of the concepts together - however, I did notice that when I was unsure or stumbled, I understood the solution/concept that was offered. 
+
+The useEffect hook caused me the most issue. I was able to (more or less) write the logic for comparing the two cards, but I didn't realize that I needed to house it in the useEffect hook. I had previously only really seen them used for data requests, so being able to understand other applications for the hook was helpful.
+
+
+
+
+
---
+++ src/App.js
@@ -0,0 +1,102 @@
+
+import { useEffect, useState } from 'react';
+import './App.css';
+import SingleCard from './Components/SingleCard';
+
+const cardImages = [
+  { src: "/images/helmet.jpg", matched: false },
+  { src: "/images/potion.jpg", matched: false },
+  { src: "/images/ring.jpg", matched: false },
+  { src: "/images/scroll.jpg", matched: false },
+  { src: "/images/shield.jpg", matched: false },
+  { src: "/images/sword.jpg", matched: false },
+]
+
+function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  //double and shuffle cards
+
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+
+      .sort(() => Math.random() - 0.5)
+      .map((prev) => ({ ...prev, id: Math.random() }))
+
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(0)
+
+  }
+
+  // handle choice
+
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+
+  }
+
+  //compare two cards
+
+  useEffect(() => {
+
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prev => {
+          return prev.map(card => {
+            if (card.src === choiceTwo.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+  }, [choiceOne, choiceTwo])
+
+
+  //reset and increase turn count
+  function resetTurn() {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prev => prev + 1)
+    setDisabled(false)
+  }
+
+  // starts game upon load
+  useEffect(() => {
+    shuffleCards()
+  }, [])
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map(card => (
+          <SingleCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled} />
+        ))}
+      </div>
+      <p>Turns: {turns}</p>
+    </div>
+  );
+}
+
+export default App;
